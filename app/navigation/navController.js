6d271ba9aa94bf49f126f30d5f8eb39f4bdd0804
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-07 13:57:57 -0500

    auth completed

--- app/navigation/navController.js
+++ app/navigation/navController.js
@@ -5,7 +5,6 @@
 app.controller("navCtrl", function($scope, $window, $location, $routeParams, $route, authFactory, getUserInfo){
 
     $scope.isLoggedIn = false;
-    $scope.isTeacher = false;
     
     $scope.logout = () => {
         authFactory.logOut();
@@ -16,20 +15,10 @@ app.controller("navCtrl", function($scope, $window, $location, $routeParams, $ro
         if (user) {
           $scope.isLoggedIn = true;
           let uid = user.uid;
-          // console.log("currentUser logged in?", user.uid);
-          getUserInfo.getUserDetails(uid)
-          .then((getUser) => {
-            let key = Object.keys(getUser);
-           $scope.isTeacher = getUser[key].isTeacher;
-          //  console.log("$scope.isTeacher", $scope.isTeacher);
-           if(getUser[key].isTeacher === true){
-            $window.location.href = "#!/admin";
-           }
-        });
+          $window.location.href = "#!/admin"
           $scope.$apply();
         } else {
           $scope.isLoggedIn = false;
-          // console.log("user logged in?", $scope.isLoggedIn);
           $window.location.href = "#!/login";
         }
       });

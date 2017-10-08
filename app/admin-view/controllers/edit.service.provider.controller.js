773f1c71a519f77428df2315d0e075e20c62309a
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-07 20:48:11 -0500

    updated add horse

--- app/admin-view/controllers/edit.service.provider.controller.js
+++ app/admin-view/controllers/edit.service.provider.controller.js
@@ -1,41 +1,43 @@
 "use strict";
 
-app.controller("EditServiceProvider", function ($scope, $location, adminEditFactory, authFactory, $routeParams, getUserInfo) {
+app.controller("EditServiceProvider", function ($scope, $location, horseFactory, adminEditFactory, authFactory, $routeParams, getUserInfo) {
     
-        $scope.newEventTitle = "Edit Event";
-        $scope.submitButtonText = "Submit Edited Event";
+        $scope.newEventTitle = "Edit Service Provider";
+        $scope.submitButtonText = "Submit Edited Provider";
         let user = authFactory.getCurrentUser();
     
-        $scope.event = {
-            eventTitle: "",
-            eventLink: "",
-            begDate: "",
-            locationName: "",
-            startTime: "",
-            locationAddy: "",
-            points: "",
-            uid: user
+        $scope.provider = {
+            address: "",
+            city: "",
+            email: "",
+            name: "",
+            phone: "",
+            provider_type: "",
+            service_provider_id: "",
+            state: "",
+            zipcode: ""
         };
 
-        const showEditEvent = function () {
-            getUserInfo.getBIGSubmittedEvent($routeParams.itemId)
-                .then((data) => {
-                    // console.log("data", data);
-                    $scope.event = data;
-                    $scope.event.begDate = new Date(data.begDate);
-                    $scope.event.startTime = new Date(data.startTime);
-                    $scope.event.id = $routeParams.itemId;
-                });
+    const showEditProvider = function() {
+        horseFactory.getSingleProvider($routeParams.itemId)
+            .then((data) => {
+                console.log ("DATA HERE!!!!", data);
+                $scope.provider = data.data;
+                $scope.provider.id = $routeParams.itemId;
+            });
         };
-        showEditEvent();
+            
+    showEditProvider();
 
-        $scope.submitNewEvent = function () {
-            adminEditFactory.updateEventAdmin($scope.event);
-            // .then((data)=>{
-            //     //$location allows to change URL path
-            //     // $location.path("#!/admin/groupsevents");
-            // });
-        };
+
+    $scope.submitNewProvider = function () {
+        console.log ("WHAT IS THIS", $scope.provider);
+        horseFactory.submitUpdatedProvider($routeParams.itemId, $scope.provider)
+        .then((data)=>{
+            //$location allows to change URL path
+            // $location.path("#!/admin/groupsevents");
+        });
+    };
         
 
 

f4c89c2f05204cd872bf386380a3155a3111c670
Gilberto Diaz <gdiaz@Gilbertos-MacBook-Pro.local>
2017-10-08 10:49:39 -0500

    add cases functionality

--- app/admin-view/controllers/allhorses.controller.js
+++ app/admin-view/controllers/allhorses.controller.js
@@ -1,15 +1,21 @@
-"use strict";
+'use strict';
 
-app.controller("allHorsesView", function($scope, horseFactory, FBCreds, authFactory){
+app.controller('allHorsesView', function($scope, horseFactory, FBCreds, authFactory) {
+  $scope.showAllHorses = () => {
+    horseFactory.getAllHorses().then(data => {
+      $scope.allHorsesData = data;
+      $scope.allHorseDataLength = data.length;
+    });
+  };
 
-    $scope.showAllHorses = () =>{
-        horseFactory.getAllHorses()
-        .then((data)=>{
-            $scope.allHorsesData = data;
-            $scope.allHorseDataLength = data.length;
-        });
-    };
+  $scope.showAllCases = () => {
+    horseFactory.getAllCases().then(data => {
+      $scope.allCases = data.data;
+      console.log('All Cases: ', $scope.allCases)
+      $scope.totalCases = Object.keys($scope.allCases)
+    });
+  };
 
-
-$scope.showAllHorses();
+  $scope.showAllHorses();
+  $scope.showAllCases();
 });

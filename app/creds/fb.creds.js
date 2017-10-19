ca5e178533da48405d1831170546d4d529b82dad
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-19 07:37:49 -0500

    stash

---
+++ app/creds/fb.creds.js
@@ -0,0 +1,11 @@
+"use strict";
+
+app.constant("FBCreds", {
+    apiKey: "AIzaSyCnThYA2kFBTvjXgAhopBI_1epnXWPB2Mk",
+    authDomain: "horse-haven-tn.firebaseapp.com",
+    databaseURL: "https://horse-haven-tn.firebaseio.com",
+    projectId: "horse-haven-tn",
+    storageBucket: "horse-haven-tn.appspot.com",
+    messagingSenderId: "653489625248"
+});
+

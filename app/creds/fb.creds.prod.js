d06a44f2ef36ece6d9807f2a60a3049285b15192
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-19 12:07:17 -0600

    updated FB creds

---
+++ app/creds/fb.creds.prod.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+// app.constant("FBCreds", {
+    // apiKey: "AIzaSyDcNqHEpCY3B_YAsGkssNrQ3oj1jjKuSx4",
+    // authDomain: "maneframe-prod.firebaseapp.com",
+    // databaseURL: "https://maneframe-prod.firebaseio.com",
+    // projectId: "maneframe-prod",
+    // storageBucket: "",
+    // messagingSenderId: "1044719678745"
+// });

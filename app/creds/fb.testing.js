a59c66049ce11de72d587a9a18e3321da427ef8f
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-19 12:24:46 -0600

    stash

--- app/creds/fb.testing.js
+++ app/creds/fb.testing.js
@@ -1,10 +1,10 @@
-"use strict";
+// "use strict";
 
-app.constant("FBCreds", {
-    apiKey: "AIzaSyCnThYA2kFBTvjXgAhopBI_1epnXWPB2Mk",
-    authDomain: "horse-haven-tn.firebaseapp.com",
-    databaseURL: "https://horse-haven-tn.firebaseio.com",
-    projectId: "horse-haven-tn",
-    storageBucket: "horse-haven-tn.appspot.com",
-    messagingSenderId: "653489625248"
-});
+// app.constant("FBCreds", {
+//     apiKey: "AIzaSyCnThYA2kFBTvjXgAhopBI_1epnXWPB2Mk",
+//     authDomain: "horse-haven-tn.firebaseapp.com",
+//     databaseURL: "https://horse-haven-tn.firebaseio.com",
+//     projectId: "horse-haven-tn",
+//     storageBucket: "horse-haven-tn.appspot.com",
+//     messagingSenderId: "653489625248"
+// });
